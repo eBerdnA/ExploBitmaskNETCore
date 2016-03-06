6a4538fd1c3c890fa096385f2ab40ebeadc89d7d
André Bering <andre@bering.in>
2016-03-06 16:37:09 +0100

    Beispiel

---
+++ src/ExploBitmaskNETCore/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploBitmaskNETCore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region from class to int/bin value
+            EditDataMode dataMode = EditDataMode.Read | EditDataMode.Create | EditDataMode.Delete;
+            Console.WriteLine("Checking flags:");
+            if (IsFlagSet(dataMode, EditDataMode.Create))
+            {
+                Console.WriteLine("  Create flag is set.");
+            }
+
+            if (IsFlagSet(dataMode, EditDataMode.Read))
+            {
+                Console.WriteLine("  Read flag is set.");
+            }
+
+            if (IsFlagSet(dataMode, EditDataMode.Update))
+            {
+                Console.WriteLine("  Update flag is set.");
+            }
+
+            if (IsFlagSet(dataMode, EditDataMode.Delete))
+            {
+                Console.WriteLine("  Delete flag is set.");
+            }
+
+            Console.WriteLine("EditDataMode to string: {0}", dataMode.ToString());
+            Console.WriteLine("From EditDataMode to int: {0}", (int)dataMode);
+            Console.WriteLine("From EditDataMode to string (bin): {0}", Convert.ToString((int)dataMode, 2));
+            Console.WriteLine("====");
+            #endregion
+
+            #region from bin/int value to class
+            string input = "0011";
+            Console.WriteLine("Input: {0}", input);
+            int convertedInput2Bin = Convert.ToInt32(input, 2);
+
+            Console.WriteLine("Converted input from string to int: {0}", convertedInput2Bin);
+            EditDataMode inputDataMode = (EditDataMode)convertedInput2Bin;
+            Console.WriteLine("EditDataMode to string: {0}", inputDataMode.ToString());
+
+            #endregion
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        static bool IsFlagSet(EditDataMode bitmask, EditDataMode flag)
+        {
+            return (bitmask & flag) != 0;
+        }
+    }
+
+    [Flags]
+    public enum EditDataMode
+    {
+        Create = 1,
+        Read = 2,
+        Update = 4,
+        Delete = 8
+    }
+}
---
+++ src/ExploBitmaskNETCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExploBitmaskNETCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GWS mbH")]
+[assembly: AssemblyProduct("ExploBitmaskNETCore")]
+[assembly: AssemblyCopyright("Copyright © GWS mbH 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b1baec6-e2b1-4a86-b27a-a3f231a97c2c")]
